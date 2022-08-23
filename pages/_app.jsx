8ddf42c9ef3f1f925a5b1fc4c60c43b5e9fd810f
pedrodruviaro <pedrodruviaro@gmail.com>
2022-08-23 08:30:07 -0300

    update: _document and _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,7 +6,23 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Theme>
-                <Head></Head>
+                <Head>
+                    <link
+                        rel="shortcut icon"
+                        href="/images/favicon.ico"
+                        type="image/png"
+                    />
+
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
 
                 <Component {...pageProps} />
             </Theme>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,26 +1,30 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-    return (
-        <Html>
-            <Head>
-                <link
-                    rel="shortcut icon"
-                    href="/images/favicon.ico"
-                    type="image/png"
-                />
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
 
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
 }
